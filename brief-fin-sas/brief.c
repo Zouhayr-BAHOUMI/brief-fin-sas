e902c4c10b9d20383c7c7b54ef67aece0c4259fe
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-01-30 11:17:58 +0100

    immplment les fonctions modifier et supprimer et trier

--- brief-fin-sas/brief.c
+++ brief-fin-sas/brief.c
@@ -20,7 +20,7 @@ typedef struct{
     char nom[30];
     char description[30];
     dateEcheance date;
-    int priorite;
+    char priorite[30];
 
 }tache;
 
@@ -41,7 +41,7 @@ typedef struct{
             scanf(" %d",&newTache->date.jour);
             printf("entrer la priorite de la tache :\n ");
             printf("1.low\t 2. medium\t 3. high\t :");
-            scanf(" %d", &newTache->priorite);
+            scanf(" %[^\n]s", newTache->priorite);
 
         }
 
@@ -65,11 +65,117 @@ typedef struct{
             printf("\n Tache N %d", i+1);
             printf("\n Nom est : %s", t[i].nom);
             printf("\n description est : %s", t[i].description);
-            printf("\n prioritie est : %d", t[i].priorite);
+            printf("\n prioritie est : %s", t[i].priorite);
             printf("\n Date d'echeance est : %d/%d/%d", t[i].date.jour, t[i].date.mois, t[i].date.annee);
             printf("\n ---------------------------------------------------------------------- \n");
         }
         }
+
+void modifierTache(tache t[],int nb){
+    char Nom[30];
+    int existe=0;
+
+    printf(" entrer le nom que vous modifier :");
+    scanf(" %[^\n]s",Nom);
+
+    for(int i=0; i<nb; i++){
+        if(strcmp(t[i].nom, Nom) == 0){
+            saisiInfos(&t[i]);
+            existe=1;
+            break;
+        }
+    }
+    if (existe == 1) {
+        printf("Modifier avec succ s.\n");
+    } else {
+        printf("somthing wrong.\n");
+    }
+}
+
+int supprimerTache( tache t[],int *nb){
+     char Nom[30];
+    int existe=0;
+
+    printf(" entrer le nom de la tache   supprimer :");
+    scanf(" %[^\n]s",Nom);
+
+    for(int i=0; i<*nb; i++){
+        if(strcmp(t[i].nom, Nom) == 0){
+            for(int j=i; j<*nb-1; j++){
+            strcpy(t[j].nom,t[j+1].nom);
+            strcpy(t[j].description,t[j+1].description);
+            strcpy(t[j].priorite,t[j+1].priorite);
+            t[j].date.jour=t[j+1].date.jour;
+            t[j].date.mois=t[j+1].date.mois;
+            t[j].date.annee=t[j+1].date.annee;
+            }
+            existe=1;
+            break;
+        }
+    }
+    if (existe == 1) {
+        printf("supprimer avec success.\n");
+        (*nb)--;
+    } else {
+        printf("somthing wrong.\n");
+    }
+    return *nb;
+}
+
+void trierTacheCroissant(tache t[], int nb){
+
+   int i,j;
+   tache temp;
+   for(i=0; i<nb; i++){
+      for(j=i+1; j<nb ; j++){
+        if( t[i].date.annee > t[j].date.annee ||  (t[i].date.annee == t[j].date.annee && t[i].date.mois > t[j].date.mois )
+            || ( t[i].date.mois == t[j].date.mois && t[i].date.jour > t[j].date.jour) ){
+            temp=t[i];
+            t[i]=t[j];
+            t[j]=temp;
+        }
+      }
+   }
+
+   printf("les tache apres trie croissant : \n");
+
+       for(i=0; i<nb ; i++){
+            printf("\n Tache N %d", i+1);
+            printf("\n Nom est : %s", t[i].nom);
+            printf("\n description est : %s", t[i].description);
+            printf("\n prioritie est : %s", t[i].priorite);
+            printf("\n Date d'echeance est : %d/%d/%d", t[i].date.jour, t[i].date.mois, t[i].date.annee);
+            printf("\n ---------------------------------------------------------------------- \n");
+       }
+}
+
+
+void trierTacheDecroissant(tache t[], int nb){
+
+   int i,j;
+   tache temp;
+   for(i=0; i<nb; i++){
+      for(j=i+1; j<nb ; j++){
+        if( t[i].date.annee < t[j].date.annee ||  (t[i].date.annee == t[j].date.annee && t[i].date.mois < t[j].date.mois )
+            || ( t[i].date.mois == t[j].date.mois && t[i].date.jour < t[j].date.jour) ){
+            temp=t[i];
+            t[i]=t[j];
+            t[j]=temp;
+        }
+      }
+   }
+
+   printf("les tache apres trie decroissant : \n");
+
+       for(i=0; i<nb ; i++){
+            printf("\n Tache N %d", i+1);
+            printf("\n Nom est : %s", t[i].nom);
+            printf("\n description est : %s", t[i].description);
+            printf("\n prioritie est : %s", t[i].priorite);
+            printf("\n Date d'echeance est : %d/%d/%d", t[i].date.jour, t[i].date.mois, t[i].date.annee);
+            printf("\n ---------------------------------------------------------------------- \n");
+       }
+}
 
 
 
@@ -104,10 +210,10 @@ int main () {
                 afficherTache(t,nbrTache);
                 break;
     case 3:
-                //modifierTache(t,nbrTache);
+                modifierTache(t,nbrTache);
                 break;
     case 4:
-                //nbrTache = supprimerTache(t,&nbrTache);
+                nbrTache = supprimerTache(t,&nbrTache);
                 break;
     case 5:
             printf("Veuillez choisir l'ordre de tri : \n");
@@ -115,13 +221,11 @@ int main () {
             printf("2. Decroissant\n");
             scanf("%d", &ordre);
             if (ordre == 1){
-               // trierTacheCroissant(t,nbrTache);
+                trierTacheCroissant(t,nbrTache);
             }else if(ordre == 2){
-               // trierTacheDecroissant(t,nbrTache);
+                trierTacheDecroissant(t,nbrTache);
             }else
                 printf("invalid choix");
-
-
             break;
     case 6:
                // filtrer();
